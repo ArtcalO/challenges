762d204f53a55b35e2b7c38800e58a0c54792d84
ArtcalO <niyca14@gmail.com>
2024-11-17 12:29:45 +0200

    remove unused code

--- Python/GoogleScrapping/scrapp_google.py
+++ Python/GoogleScrapping/scrapp_google.py
@@ -1,7 +1,5 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import sys
 import locale
 from fake_useragent import UserAgent
@@ -64,8 +62,6 @@ def analyse_site(url):
     # Utiliser BeautifulSoup pour parser le code HTML
     soup = BeautifulSoup(html_content, 'html.parser')
 
-
-
     requests_delay()
     results = soup.find_all("div", class_="Nv2PK THOPZb CpccDe")
     scrapped_items = []
@@ -84,68 +80,6 @@ def analyse_site(url):
         scrapped_items.append(data)
 
     print(scrapped_items)
-    # adresse = p_adresse.find("a").text.strip()
 
-    # h1_title = soup.find("h1", class_="text-headline-1-expanded u-break-word")
-    # title = h1_title.text.strip()
-
-    # prix_str = soup.find("p", class_="text-headline-2").text.strip().split("€")[0]
-    # locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
-    # prix = locale.atoi(prix_str)
-
-    # info_list = []
-
-    # elements = soup.find_all("div", id="ad_param_truncate_text")
-
-    # for elt in elements :
-    #    p_elt = elt.find("p").text.strip()
-    #    info_list.append(p_elt)
-
-    # type_habitat = info_list[0]
-
-    # surface_habitable = int(info_list[1].split('m²')[0])
-
-    # print(info_list)
-
-    # if type_habitat == 'Appartement':
-    #     pieces = int(info_list[2]) 
-    # else:
-    #     if type_habitat == 'Maison':
-    #         pieces = int(info_list[3]) 
-    #     else:
-    #         pieces = None
-    
-
-    # div_energy = soup.find_all("div", class_="drop-shadow")
-
-    # if(len(div_energy) > 1):
-    #     dpe = div_energy[0].text.strip()
-    #     ges = div_energy[1].text.strip()
-    # else:
-    #     dpe = None
-    #     ges = None
-
-    # p_description = soup.find("p", class_="whitespace-pre-line text-body-1 [overflow-wrap:anywhere] line-clamp-6").text.strip().replace("\n", " ")
-
-    # #requests_delay()
-
-    # # Construire la réponse JSON
-    # response = {
-    #     'adresse':adresse,
-    #     'url_img': url_img,
-    #     'title': title,
-    #     'prix':prix,
-    #     'type_habitat':type_habitat,
-    #     'surface_habitable':surface_habitable,
-    #     'nbr_pieces':pieces,
-    #     "ges": ges,
-    #     "dpe": dpe,
-    #     'description': p_description
-        
-    # }
-
-    # with open('data.json', 'w') as outfile:
-    #     json.dump(response, outfile)
-    # return response
 
 info = analyse_site("https://www.google.com/maps/search/restaurants/")
